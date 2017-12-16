90c107b08c9bf624e26a7db596cf10dbebd43b69
voidsun <voidsun@icloud.com>
2017-12-16 15:47:12 +0800

    Site updated: 2017-12-16 15:47:10

--- js/src/dynamic-title.js
+++ js/src/dynamic-title.js
@@ -3,11 +3,11 @@ var OriginTitile = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
        if (document.hidden) {
-                $('[rel="icon"]').attr('href', "/img/TEP.ico");
+                $('[rel="icon"]').attr('href', "/images/favicon.ico");
 		document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
 		clearTimeout(titleTime);
 	} else {
-	        $('[rel="icon"]').attr('href', "/favicon.ico");
+	        $('[rel="icon"]').attr('href', "/images/favicon.ico");
 	        document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
 	        titleTime = setTimeout(function () {
 	                document.title = OriginTitile;
